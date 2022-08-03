50758fd905a3b11b78f575b926b3b5cb8d5d5875
d7o1d1s0 <d7sarmy@gmail.com>
2022-08-03 15:16:34 +0100

    token components

---
+++ src/containers/TokenContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TokenChart from '../components/TokenChart';
+
+const TokenContainer = () => {
+
+    return (
+    <>
+        <h2>Token</h2>
+        <TokenChart></TokenChart>
+    </>
+    )
+}
+
+export default TokenContainer;
