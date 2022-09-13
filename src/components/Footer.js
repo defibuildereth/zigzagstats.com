d84c87b2cbca001010c77c182be53a3b71bd0d2f
d7o1d1s0 <d7sarmy@gmail.com>
2022-09-13 16:31:34 +0100

    local image, links

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = ({ }) => {
+
+
+    return (
+        <><div className='fixed bottom-0 flex flex-col'>
+            <a className='text-xs' href='https://defibuilder.eth.limo/'>DefiBuilder Blog</a>
+            <a className='text-xs' href='https://www.zkscanalyzer.com/'>ZkScanalyzer.com</a>
+            <a className='text-xs' href='https://www.defiyieldcalculator.com/'>DefiYieldCalculator.com</a>
+        </div>
+        </>
+    )
+}
+
+export default Footer
